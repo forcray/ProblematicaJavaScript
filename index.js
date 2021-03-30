37c60b3ecbc58be5ab8f5a07ac6f57bbb2c25766
forcray <forcray@gmail.com>
2021-03-30 01:57:30 -0300

    Variables y Operadores

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
+/* 
+1)	Variables y Operadores
+a)	Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+b)	Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+c)	Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+*/
+
 console.log ('Executing external script made with JS')
 
-var myVariable
-var x = 2;
-var y = 3;
-console.log('variables', myVariable, x, y);
+var x, y;
+x = 11;
+y = 4;
 
+var resultado = x + y;
 
+console.log('Resultado de la suma de los dos:' + resultado);
+
+var cholo, enzo;
+var cholo = '-hola que hace como anda ';
+var enzo = '-hola si todo piola';
+var joined = cholo + enzo;
+var cantidadLetras;
+joined;
+
+console.log(cholo,enzo)
+console.log('conversacion rioba:' + joined);
+console.log('Cantidad de letras es: ' + joined.length + ' muchas letras ¿no te parece?');
+
+/* 2)	Strings
+a)	Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
+var abcdefghij
+toUpperCase
+/*b)	Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+/*c)	Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+/*d)	Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+
+/*e)	Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+/*f)	Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
